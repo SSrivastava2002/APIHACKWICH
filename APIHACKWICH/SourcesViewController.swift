53ece0c0df3104e4db7be1cfa926e0e213b62bfd
Shubham Srivastava <ssrivastava1@shubhams-macbook-air-3.local>
2019-02-21 11:33:13 -0600

    finished background data loading routine

--- APIHACKWICH/SourcesViewController.swift
+++ APIHACKWICH/SourcesViewController.swift
@@ -12,21 +12,25 @@ class SourcesViewController: UITableViewController {
     var sources = [[String: String]]()
     let apiKey = "5d892509a49046a087917c466fa80d09"
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "News Sources"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
-        if let url = URL(string: query) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON(data: data)
-                if json["status"] == "ok" {
-                    parse(json: json)
-                    return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    if json["status"] == "ok" {
+                        self.parse(json: json)
+                        return
+                    }
                 }
+                
             }
+            self.loadError()
         }
-        loadError() 
     }
     
     func parse(json: JSON){
@@ -34,31 +38,39 @@ class SourcesViewController: UITableViewController {
             let id = result["id"].stringValue
             let name = result["name"].stringValue
             let description = result["description"].stringValue
-             let source = ["id": id, "name": name, "description": description]
+            let source = ["id": id, "name": name, "description": description]
             sources.append(source)
         }
-           tableView.reloadData()
+        DispatchQueue.main.async {
+            [unowned self] in
+            self.tableView.reloadData()
+        }
     }
     
     func loadError() {
-        let alert = UIAlertController(title: "Loading Error",
-                                      message: "There was a problem loading the news feed",
-                                      preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil) }
+        DispatchQueue.main.async {
+            [unowned self] in
+            
+            let alert = UIAlertController(title: "Loading Error",
+                                          message: "There was a problem loading the news feed",
+                                          preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sources.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier( "cell", indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let source = sources[indexPath.row]
         cell.textLabel?.text = source["name"]
         cell.detailTextLabel?.text = source["description"]
         return cell
-
+        
     }
-
-
+    
+    
 }
 
